22100f42b15250f293b371c27ebeb0bae5e40f93
Alnoman141 <nmnaba14@gmail.com>
2022-06-28 01:23:12 +0600

    Settings Link and Admin Pages done

--- wp-content/plugins/neer-plugin/neer-plugin.php
+++ wp-content/plugins/neer-plugin/neer-plugin.php
@@ -45,12 +45,33 @@ if(!function_exists('add_action')){
 }
 
 class NeerPlugin {
+
+    public $plugin;
+
     function __construct(){
+        $this->plugin = plugin_basename(__FILE__);
         add_action('init', array($this, 'custom_post_type'));
+        add_action('admin_menu', array($this, 'add_admin_menu'));
+        
+        add_filter("plugin_action_links_$this->plugin", array($this, 'settings_link'));
+    }
+
+    public function settings_link($links){
+        $link = '<a href="admin.php?page=neer_plugin" >Settings</a>';
+        array_push($links, $link);
+        return $links;
     }
 
     function register(){
         add_action('wp_enqueue_scripts', array($this, 'enqueue'));
+    }
+
+    public function add_admin_menu(){
+        add_menu_page('Neer Plugin', 'Neer Plugin', 'manage_options', 'neer_plugin', array($this, 'admin_index'), 'dashicons-editor-paste-word', 110);
+    }
+
+    public function admin_index(){
+        require_once plugin_dir_path(__FILE__). 'templates/admin.php';
     }
 
     function activation(){
